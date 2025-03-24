609fbd5f477786f333160e35580ea13e5b23b62d
vxxxxc <>
2025-03-25 02:20:01 +0800

    create api handler of new todo item

---
+++ src/api_handlers/todo_handler.rs
@@ -0,0 +1,10 @@
+use actix_web::{web, HttpResponse};
+use crate::api_types::todo_types::Todo;
+
+pub async fn handle_todo(todo: web::Json<Todo>) -> HttpResponse {
+    let new_todo = Todo {
+        item: todo.item.clone(),
+    };
+
+    HttpResponse::Created().json(new_todo)
+}
